5cdcaea1edbc20edbee1d2471b625409f0bd60c2
kevL <k3v1s73n@gmail.com>
2020-12-15 12:18:59 -0700

    InfoVersion - dispose inputbox

--- InfoVersion.cs
+++ InfoVersion.cs
@@ -44,80 +44,82 @@ namespace nwn2_ai_2da_editor
 					break;
 			}
 
+			DialogResult dr = DialogResult.Cancel;
 
-			var ib = new Form();
+			using (var ib = new Form())
+			{
+				var size = new Size(350, 125);
 
-			var size = new Size(350, 125);
-
-			ib.FormBorderStyle = FormBorderStyle.FixedToolWindow;
-			ib.StartPosition = FormStartPosition.CenterParent;
-			ib.ClientSize = size;
-			ib.Text = title;
-			ib.Font = new Font("Courier New", 8.25f, FontStyle.Regular, GraphicsUnit.Point, (byte)0);
-
-
-			const int pad = 5;
-
-			var infoText = new Label();
-			infoText.Size = new Size(size.Width - pad * 2, 20);
-			infoText.Location = new Point(pad, pad + 2);
-			infoText.Font = new Font("Courier New", 8.25f, FontStyle.Bold, GraphicsUnit.Point, (byte)0);
-			infoText.ForeColor = Color.Crimson;
-			infoText.TextAlign = ContentAlignment.TopCenter;
-			infoText.Text = "Do NOT change this.";
-			ib.Controls.Add(infoText);
-
-			var textBox = new TextBox();
-			textBox.Size = new Size(size.Width - pad * 10, 20);
-			textBox.Location = new Point(pad * 5, 30);
-			textBox.Text = str;
-			textBox.TextChanged += TextChanged_ver;
-			ib.Controls.Add(textBox);
-
-			const int wBtn = 80;
-			const int hBtn = 55;
-			int yBtn = size.Height - hBtn - pad;
+				ib.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+				ib.StartPosition = FormStartPosition.CenterParent;
+				ib.ClientSize = size;
+				ib.Text = title;
+				ib.Font = new Font("Courier New", 8.25f, FontStyle.Regular, GraphicsUnit.Point, (byte)0);
 
 
-			var cancelButton = new Button();
-			cancelButton.DialogResult = DialogResult.Cancel;
-			cancelButton.Name = "cancelButton";
-			cancelButton.Size = new Size(wBtn, hBtn);
-			cancelButton.Text = "&Cancel";
-			cancelButton.Location = new Point((size.Width - pad) * 4 / 4 - wBtn, yBtn);
-			ib.Controls.Add(cancelButton);
+				const int pad = 5;
 
-			var okButton = new Button();
-			okButton.DialogResult = DialogResult.OK;
-			okButton.Name = "okButton";
-			okButton.Size = new Size(wBtn, hBtn);
-			okButton.Text = text_1;
-			okButton.Location = new Point((size.Width - pad) * 3 / 4 - wBtn, yBtn);
-			ib.Controls.Add(okButton);
+				var infoText = new Label();
+				infoText.Size = new Size(size.Width - pad * 2, 20);
+				infoText.Location = new Point(pad, pad + 2);
+				infoText.Font = new Font("Courier New", 8.25f, FontStyle.Bold, GraphicsUnit.Point, (byte)0);
+				infoText.ForeColor = Color.Crimson;
+				infoText.TextAlign = ContentAlignment.TopCenter;
+				infoText.Text = "Do NOT change this!";
+				ib.Controls.Add(infoText);
 
-			var anyButton = new Button();
-			anyButton.DialogResult = DialogResult.Yes;
-			anyButton.Name = "anyButton";
-			anyButton.Size = new Size(wBtn, hBtn);
-			anyButton.Text = text_2;
-			anyButton.Location = new Point((size.Width - pad) * 2 / 4 - wBtn, yBtn);
-			ib.Controls.Add(anyButton);
+				var textBox = new TextBox();
+				textBox.Size = new Size(size.Width - pad * 10, 20);
+				textBox.Location = new Point(pad * 5, 30);
+				textBox.Text = str;
+				textBox.TextChanged += TextChanged_ver;
+				ib.Controls.Add(textBox);
 
-			var allButton = new Button();
-			allButton.DialogResult = DialogResult.Retry;
-			allButton.Name = "allButton";
-			allButton.Size = new Size(wBtn, hBtn);
-			allButton.Text = text_3;
-			allButton.Location = new Point((size.Width - pad) * 1 / 4 - wBtn, yBtn);
-			ib.Controls.Add(allButton);
+				const int wBtn = 80;
+				const int hBtn = 55;
+				int yBtn = size.Height - hBtn - pad;
 
 
-			ib.AcceptButton = okButton;		// [Enter]
-			ib.CancelButton = cancelButton;	// [Esc]
+				var cancelButton = new Button();
+				cancelButton.DialogResult = DialogResult.Cancel;
+				cancelButton.Name = "cancelButton";
+				cancelButton.Size = new Size(wBtn, hBtn);
+				cancelButton.Text = "&Cancel";
+				cancelButton.Location = new Point((size.Width - pad) * 4 / 4 - wBtn, yBtn);
+				ib.Controls.Add(cancelButton);
 
-			DialogResult dr = ib.ShowDialog();
+				var okButton = new Button();
+				okButton.DialogResult = DialogResult.OK;
+				okButton.Name = "okButton";
+				okButton.Size = new Size(wBtn, hBtn);
+				okButton.Text = text_1;
+				okButton.Location = new Point((size.Width - pad) * 3 / 4 - wBtn, yBtn);
+				ib.Controls.Add(okButton);
 
-			str = textBox.Text;
+				var anyButton = new Button();
+				anyButton.DialogResult = DialogResult.Yes;
+				anyButton.Name = "anyButton";
+				anyButton.Size = new Size(wBtn, hBtn);
+				anyButton.Text = text_2;
+				anyButton.Location = new Point((size.Width - pad) * 2 / 4 - wBtn, yBtn);
+				ib.Controls.Add(anyButton);
+
+				var allButton = new Button();
+				allButton.DialogResult = DialogResult.Retry;
+				allButton.Name = "allButton";
+				allButton.Size = new Size(wBtn, hBtn);
+				allButton.Text = text_3;
+				allButton.Location = new Point((size.Width - pad) * 1 / 4 - wBtn, yBtn);
+				ib.Controls.Add(allButton);
+
+
+				ib.AcceptButton = okButton;		// [Enter]
+				ib.CancelButton = cancelButton;	// [Esc]
+
+				dr = ib.ShowDialog();
+
+				str = textBox.Text;
+			}
 			return dr;
 		}
 
