8a69bf7f4b268952d0d0490060b9af89dc3fc464
kevL <k3v1s73n@gmail.com>
2020-12-19 13:37:10 -0700

    add extensions.cs to /util
    move controls.cs to /util

--- funct_bar.cs
+++ funct_bar.cs
@@ -1462,36 +1462,4 @@ namespace nwn2_ai_2da_editor
 		}
 		#endregion Write
 	}
-
-
-
-	/// <summary>
-	/// Lifted from StackOverflow.com:
-	/// https://stackoverflow.com/questions/1600962/displaying-the-build-date#answer-1600990
-	/// </summary>
-	public static class DateTimeExtension
-	{
-		public static DateTime GetLinkerTime(this Assembly assembly, TimeZoneInfo target = null)
-		{
-			var filePath = assembly.Location;
-			const int c_PeHeaderOffset = 60;
-			const int c_LinkerTimestampOffset = 8;
-
-			var buffer = new byte[2048];
-
-			using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-				stream.Read(buffer, 0, 2048);
-
-			var offset = BitConverter.ToInt32(buffer, c_PeHeaderOffset);
-			var secondsSince1970 = BitConverter.ToInt32(buffer, offset + c_LinkerTimestampOffset);
-			var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
-			var linkTimeUtc = epoch.AddSeconds(secondsSince1970);
-
-			var tz = target ?? TimeZoneInfo.Local;
-			var localTime = TimeZoneInfo.ConvertTimeFromUtc(linkTimeUtc, tz);
-
-			return localTime;
-		}
-	}
 }
