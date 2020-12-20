7cd823ee9437ca4a792c06c51652754e2501c303
kevL <k3v1s73n@gmail.com>
2020-12-20 16:01:48 -0700

    add class TextboxInt - numeric only textbox

--- util/controls.cs
+++ util/controls.cs
@@ -48,4 +48,53 @@ namespace nwn2_ai_2da_editor
 		}
 		#endregion Properties (override)
 	}
+
+
+	/// <summary>
+	/// Derived class for TextBox. Allows integers only in the textbox.
+	/// </summary>
+	sealed class TextboxInt
+		: TextBox
+	{
+		string _pre = String.Empty;
+
+		protected override void OnTextChanged(EventArgs e)
+		{
+			bool retry = !String.IsNullOrEmpty(Text)	// allow blank string
+					  && Text != "-";					// allow "-"
+
+			if (retry)
+			{
+				int result;
+				retry = !Int32.TryParse(Text, out result);
+			}
+
+			if (retry)
+			{
+				Text = _pre; // revert & recurse
+				SelectionLength = 0;
+				SelectionStart = Text.Length;
+			}
+			else
+			{
+				_pre = Text;
+				base.OnTextChanged(e);
+			}
+		}
+
+		protected override void OnLeave(EventArgs e)
+		{
+			string text = Text.Trim().TrimStart('0');
+
+			int result;
+			if (!Int32.TryParse(text, out result)
+				|| result == 0) // handle unusual cases incl/ "", "-", "-0", "00", etc.
+			{
+				text = "0";
+			}
+
+			if (text != Text)
+				Text = text;
+		}
+	}
 }
