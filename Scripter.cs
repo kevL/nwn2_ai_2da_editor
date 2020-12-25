d69ea1080acd4ed4131a471dcd1644dae0c84f0c
kevL <k3v1s73n@gmail.com>
2020-12-24 23:55:42 -0700

    Scripter - auto extended info for fields

--- Scripter.cs
+++ Scripter.cs
@@ -13,11 +13,70 @@ namespace nwn2_ai_2da_editor
 		#region Fields (static)
 		static int _x = -1, _y, _w, _h;
 		static int _p = -1;
+
+		// MetaMagic
+//		const int META_NONE               = 0x00000000; //        0
+		const int META_EMPOWER            = 0x00000001; //        1
+		const int META_EXTEND             = 0x00000002; //        2
+		const int META_MAXIMIZE           = 0x00000004; //        4
+		const int META_QUICKEN            = 0x00000008; //        8
+		const int META_SILENT             = 0x00000010; //       16
+		const int META_STILL              = 0x00000020; //       32
+		const int META_PERSISTENT         = 0x00000040; //       64
+		const int META_PERMANENT          = 0x00000080; //      128
+
+		const int META_I_DRAINING_BLAST   = 0x00000100; //      256
+		const int META_I_ELDRITCH_SPEAR   = 0x00000200; //      512
+		const int META_I_FRIGHTFUL_BLAST  = 0x00000400; //     1024
+		const int META_I_HIDEOUS_BLOW     = 0x00000800; //     2048
+		const int META_I_BESHADOWED_BLAST = 0x00001000; //     4096
+		const int META_I_BRIMSTONE_BLAST  = 0x00002000; //     8192
+		const int META_I_ELDRITCH_CHAIN   = 0x00004000; //    16384
+		const int META_I_HELLRIME_BLAST   = 0x00008000; //    32768
+		const int META_I_BEWITCHING_BLAST = 0x00010000; //    65536
+		const int META_I_ELDRITCH_CONE    = 0x00020000; //   131072
+		const int META_I_NOXIOUS_BLAST    = 0x00040000; //   262144
+		const int META_I_VITRIOLIC_BLAST  = 0x00080000; //   524288
+		const int META_I_ELDRITCH_DOOM    = 0x00100000; //  1048576
+		const int META_I_UTTERDARK_BLAST  = 0x00200000; //  2097152
+		const int META_I_HINDERING_BLAST  = 0x00400000; //  4194304
+		const int META_I_BINDING_BLAST    = 0x00800000; //  8388608
 		#endregion Fields (static)
 
 
 		#region Fields
 		List<string> _fieldcols = new List<string>();
+
+		int FIELD_School            = -1;
+		int FIELD_Range             = -1;
+		int FIELD_VS                = -1;
+		int FIELD_MetaMagic         = -1;
+//		int FIELD_TargetType        = -1;
+		int FIELD_Bard              = -1;
+		int FIELD_Cleric            = -1;
+		int FIELD_Druid             = -1;
+		int FIELD_Paladin           = -1;
+		int FIELD_Ranger            = -1;
+		int FIELD_Wiz_Sorc          = -1;
+		int FIELD_Warlock           = -1;
+		int FIELD_Innate            = -1;
+		int FIELD_ImmunityType      = -1;
+		int FIELD_ItemImmunity      = -1;
+		int FIELD_SubRadSpell1      = -1;
+		int FIELD_SubRadSpell2      = -1;
+		int FIELD_SubRadSpell3      = -1;
+		int FIELD_SubRadSpell4      = -1;
+		int FIELD_SubRadSpell5      = -1;
+		int FIELD_Category          = -1;
+		int FIELD_Master            = -1;
+		int FIELD_UserType          = -1;
+		int FIELD_UsesConcentration = -1;
+		int FIELD_SpontaneouslyCast = -1;
+		int FIELD_SpontCastClassReq = -1;
+		int FIELD_HostileSetting    = -1;
+		int FIELD_FeatID            = -1;
+		int FIELD_AsMetaMagic       = -1;
+		int FIELD_REMOVED           = -1;
 		#endregion Fields
 
 
@@ -131,101 +190,96 @@ namespace nwn2_ai_2da_editor
 				}
 
 				int result;
-				switch (i)
+				if (i == FIELD_MetaMagic) // NOTE: switch() uses constant vals only.
 				{
-					case 3: // MetaMagic
-						if (fields[i].Length > 2 && fields[i].Substring(0,2) == "0x"
-							&& Int32.TryParse(fields[i].Substring(2),
-											  NumberStyles.AllowHexSpecifier, // <- that treats the string as hexadecimal notation
-											  CultureInfo.InvariantCulture,   //    but does *not* allow the hex-specifier "0x"
-											  out result))
+					if (fields[i].Length > 2 && fields[i].Substring(0,2) == "0x"
+						&& Int32.TryParse(fields[i].Substring(2),
+										  NumberStyles.AllowHexSpecifier, // <- that treats the string as hexadecimal notation
+										  CultureInfo.InvariantCulture,   //    but does *not* allow the hex-specifier "0x"
+										  out result))
+					{
+						// standard ->
+						if ((result & META_EMPOWER)            != 0) line += Environment.NewLine + " empower";
+						if ((result & META_EXTEND)             != 0) line += Environment.NewLine + " extend";
+						if ((result & META_MAXIMIZE)           != 0) line += Environment.NewLine + " maximize";
+						if ((result & META_QUICKEN)            != 0) line += Environment.NewLine + " quicken";
+						if ((result & META_SILENT)             != 0) line += Environment.NewLine + " silent";
+						if ((result & META_STILL)              != 0) line += Environment.NewLine + " still";
+						if ((result & META_PERSISTENT)         != 0) line += Environment.NewLine + " persistent";
+						if ((result & META_PERMANENT)          != 0) line += Environment.NewLine + " permanent";
+
+						// Eldritch Essences ->
+						if ((result & META_I_BESHADOWED_BLAST) != 0) line += Environment.NewLine + " beshadowed blast";
+						if ((result & META_I_BEWITCHING_BLAST) != 0) line += Environment.NewLine + " bewitching blast";
+						if ((result & META_I_BINDING_BLAST)    != 0) line += Environment.NewLine + " binding blast";
+						if ((result & META_I_BRIMSTONE_BLAST)  != 0) line += Environment.NewLine + " brimstone blast";
+						if ((result & META_I_DRAINING_BLAST)   != 0) line += Environment.NewLine + " draining blast";
+						if ((result & META_I_FRIGHTFUL_BLAST)  != 0) line += Environment.NewLine + " frightful blast";
+						if ((result & META_I_HELLRIME_BLAST)   != 0) line += Environment.NewLine + " hellrime blast";
+						if ((result & META_I_HINDERING_BLAST)  != 0) line += Environment.NewLine + " hindering blast";
+						if ((result & META_I_NOXIOUS_BLAST)    != 0) line += Environment.NewLine + " noxious blast";
+						if ((result & META_I_UTTERDARK_BLAST)  != 0) line += Environment.NewLine + " utterdark blast";
+						if ((result & META_I_VITRIOLIC_BLAST)  != 0) line += Environment.NewLine + " vitriolic blast";
+
+						// Blast Shapes ->
+						if ((result & META_I_ELDRITCH_CHAIN)   != 0) line += Environment.NewLine + " eldritch chain";
+						if ((result & META_I_ELDRITCH_CONE)    != 0) line += Environment.NewLine + " eldritch cone";
+						if ((result & META_I_ELDRITCH_DOOM)    != 0) line += Environment.NewLine + " eldritch doom";
+						if ((result & META_I_ELDRITCH_SPEAR)   != 0) line += Environment.NewLine + " eldritch spear";
+						if ((result & META_I_HIDEOUS_BLOW)     != 0) line += Environment.NewLine + " hideous blow";
+					}
+				}
+				else if (i == FIELD_Category)
+				{
+					if (Int32.TryParse(fields[i], out result)
+						&& result > -1 && result < 26)
+					{
+						line += Environment.NewLine;
+						switch (result)
 						{
-							// standard ->
-							if ((result & META_EMPOWER)            != 0) line += Environment.NewLine + "  empower";
-							if ((result & META_EXTEND)             != 0) line += Environment.NewLine + "  extend";
-							if ((result & META_MAXIMIZE)           != 0) line += Environment.NewLine + "  maximize";
-							if ((result & META_QUICKEN)            != 0) line += Environment.NewLine + "  quicken";
-							if ((result & META_SILENT)             != 0) line += Environment.NewLine + "  silent";
-							if ((result & META_STILL)              != 0) line += Environment.NewLine + "  still";
-							if ((result & META_PERSISTENT)         != 0) line += Environment.NewLine + "  persistent";
-							if ((result & META_PERMANENT)          != 0) line += Environment.NewLine + "  permanent";
-
-							// Eldritch Essences ->
-							if ((result & META_I_BESHADOWED_BLAST) != 0) line += Environment.NewLine + "  beshadowed blast";
-							if ((result & META_I_BEWITCHING_BLAST) != 0) line += Environment.NewLine + "  bewitching blast";
-							if ((result & META_I_BINDING_BLAST)    != 0) line += Environment.NewLine + "  binding blast";
-							if ((result & META_I_BRIMSTONE_BLAST)  != 0) line += Environment.NewLine + "  brimstone blast";
-							if ((result & META_I_DRAINING_BLAST)   != 0) line += Environment.NewLine + "  draining blast";
-							if ((result & META_I_FRIGHTFUL_BLAST)  != 0) line += Environment.NewLine + "  frightful blast";
-							if ((result & META_I_HELLRIME_BLAST)   != 0) line += Environment.NewLine + "  hellrime blast";
-							if ((result & META_I_HINDERING_BLAST)  != 0) line += Environment.NewLine + "  hindering blast";
-							if ((result & META_I_NOXIOUS_BLAST)    != 0) line += Environment.NewLine + "  noxious blast";
-							if ((result & META_I_UTTERDARK_BLAST)  != 0) line += Environment.NewLine + "  utterdark blast";
-							if ((result & META_I_VITRIOLIC_BLAST)  != 0) line += Environment.NewLine + "  vitriolic blast";
-
-							// Blast Shapes ->
-							if ((result & META_I_ELDRITCH_CHAIN)   != 0) line += Environment.NewLine + "  eldritch chain";
-							if ((result & META_I_ELDRITCH_CONE)    != 0) line += Environment.NewLine + "  eldritch cone";
-							if ((result & META_I_ELDRITCH_DOOM)    != 0) line += Environment.NewLine + "  eldritch doom";
-							if ((result & META_I_ELDRITCH_SPEAR)   != 0) line += Environment.NewLine + "  eldritch spear";
-							if ((result & META_I_HIDEOUS_BLOW)     != 0) line += Environment.NewLine + "  hideous blow";
+							case  0: line += " none";                              break;
+							case  1: line += " HARMFUL_AREAEFFECT_DISCRIMINANT";   break;
+							case  2: line += " HARMFUL_RANGED";                    break;
+							case  3: line += " HARMFUL_TOUCH";                     break;
+							case  4: line += " BENEFICIAL_HEALING_AREAEFFECT";     break;
+							case  5: line += " BENEFICIAL_HEALING_TOUCH";          break;
+							case  6: line += " BENEFICIAL_CONDITIONAL_AREAEFFECT"; break;
+							case  7: line += " BENEFICIAL_CONDITIONAL_SINGLE";     break;
+							case  8: line += " BENEFICIAL_ENHANCEMENT_AREAEFFECT"; break;
+							case  9: line += " BENEFICIAL_ENHANCEMENT_SINGLE";     break;
+							case 10: line += " BENEFICIAL_ENHANCEMENT_SELF";       break;
+							case 11: line += " HARMFUL_AREAEFFECT_INDISCRIMINANT"; break;
+							case 12: line += " BENEFICIAL_PROTECTION_SELF";        break;
+							case 13: line += " BENEFICIAL_PROTECTION_SINGLE";      break;
+							case 14: line += " BENEFICIAL_PROTECTION_AREAEFFECT";  break;
+							case 15: line += " BENEFICIAL_OBTAIN_ALLIES";          break;
+							case 16: line += " PERSISTENT_AREA_OF_EFFECT";         break;
+							case 17: line += " BENEFICIAL_HEALING_POTION";         break;
+							case 18: line += " BENEFICIAL_CONDITIONAL_POTION";     break;
+							case 19: line += " DRAGONS_BREATH";                    break;
+							case 20: line += " BENEFICIAL_PROTECTION_POTION";      break;
+							case 21: line += " BENEFICIAL_ENHANCEMENT_POTION";     break;
+							case 22: line += " HARMFUL_MELEE";                     break;
+							case 23: line += " DISPEL";                            break;
+							case 24: line += " SPELLBREACH";                       break;
+							case 25: line += " CANTRIP";                           break;
 						}
-						break;
-
-//					case 4: // TargetType
-//						break;
-
-					case 20: // Category
-						if (Int32.TryParse(fields[i], out result)
-							&& result > -1 && result < 26)
+					}
+				}
+				else if (i == FIELD_UserType)
+				{
+					if (Int32.TryParse(fields[i], out result)
+						&& result > 0 && result < 5)
+					{
+						line += Environment.NewLine;
+						switch (result)
 						{
-							line += Environment.NewLine;
-							switch (result)
-							{
-								case  0: line += "  none";                              break;
-								case  1: line += "  HARMFUL_AREAEFFECT_DISCRIMINANT";   break;
-								case  2: line += "  HARMFUL_RANGED";                    break;
-								case  3: line += "  HARMFUL_TOUCH";                     break;
-								case  4: line += "  BENEFICIAL_HEALING_AREAEFFECT";     break;
-								case  5: line += "  BENEFICIAL_HEALING_TOUCH";          break;
-								case  6: line += "  BENEFICIAL_CONDITIONAL_AREAEFFECT"; break;
-								case  7: line += "  BENEFICIAL_CONDITIONAL_SINGLE";     break;
-								case  8: line += "  BENEFICIAL_ENHANCEMENT_AREAEFFECT"; break;
-								case  9: line += "  BENEFICIAL_ENHANCEMENT_SINGLE";     break;
-								case 10: line += "  BENEFICIAL_ENHANCEMENT_SELF";       break;
-								case 11: line += "  HARMFUL_AREAEFFECT_INDISCRIMINANT"; break;
-								case 12: line += "  BENEFICIAL_PROTECTION_SELF";        break;
-								case 13: line += "  BENEFICIAL_PROTECTION_SINGLE";      break;
-								case 14: line += "  BENEFICIAL_PROTECTION_AREAEFFECT";  break;
-								case 15: line += "  BENEFICIAL_OBTAIN_ALLIES";          break;
-								case 16: line += "  PERSISTENT_AREA_OF_EFFECT";         break;
-								case 17: line += "  BENEFICIAL_HEALING_POTION";         break;
-								case 18: line += "  BENEFICIAL_CONDITIONAL_POTION";     break;
-								case 19: line += "  DRAGONS_BREATH";                    break;
-								case 20: line += "  BENEFICIAL_PROTECTION_POTION";      break;
-								case 21: line += "  BENEFICIAL_ENHANCEMENT_POTION";     break;
-								case 22: line += "  HARMFUL_MELEE";                     break;
-								case 23: line += "  DISPEL";                            break;
-								case 24: line += "  SPELLBREACH";                       break;
-								case 25: line += "  CANTRIP";                           break;
-							}
+							case 1: line += " spell";           break;
+							case 2: line += " special ability"; break;
+							case 3: line += " feat";            break;
+							case 4: line += " item power";      break;
 						}
-						break;
-
-					case 22: // UserType
-						if (Int32.TryParse(fields[i], out result)
-							&& result > 0 && result < 5)
-						{
-							line += Environment.NewLine;
-							switch (result)
-							{
-								case 1: line += "  spell";           break;
-								case 2: line += "  special ability"; break;
-								case 3: line += "  feat";            break;
-								case 4: line += "  item power";      break;
-							}
-						}
-						break;
+					}
 				}
 
 				if (text != String.Empty) text += Environment.NewLine;
@@ -233,35 +287,6 @@ namespace nwn2_ai_2da_editor
 			}
 			tb_Fields.Text = text;
 		}
-
-		// MetaMagic
-		internal const int META_NONE               = 0x00000000; //        0
-		internal const int META_EMPOWER            = 0x00000001; //        1
-		internal const int META_EXTEND             = 0x00000002; //        2
-		internal const int META_MAXIMIZE           = 0x00000004; //        4
-		internal const int META_QUICKEN            = 0x00000008; //        8
-		internal const int META_SILENT             = 0x00000010; //       16
-		internal const int META_STILL              = 0x00000020; //       32
-		internal const int META_PERSISTENT         = 0x00000040; //       64
-		internal const int META_PERMANENT          = 0x00000080; //      128
-
-		internal const int META_I_DRAINING_BLAST   = 0x00000100; //      256
-		internal const int META_I_ELDRITCH_SPEAR   = 0x00000200; //      512
-		internal const int META_I_FRIGHTFUL_BLAST  = 0x00000400; //     1024
-		internal const int META_I_HIDEOUS_BLOW     = 0x00000800; //     2048
-		internal const int META_I_BESHADOWED_BLAST = 0x00001000; //     4096
-		internal const int META_I_BRIMSTONE_BLAST  = 0x00002000; //     8192
-		internal const int META_I_ELDRITCH_CHAIN   = 0x00004000; //    16384
-		internal const int META_I_HELLRIME_BLAST   = 0x00008000; //    32768
-		internal const int META_I_BEWITCHING_BLAST = 0x00010000; //    65536
-		internal const int META_I_ELDRITCH_CONE    = 0x00020000; //   131072
-		internal const int META_I_NOXIOUS_BLAST    = 0x00040000; //   262144
-		internal const int META_I_VITRIOLIC_BLAST  = 0x00080000; //   524288
-		internal const int META_I_ELDRITCH_DOOM    = 0x00100000; //  1048576
-		internal const int META_I_UTTERDARK_BLAST  = 0x00200000; //  2097152
-		internal const int META_I_HINDERING_BLAST  = 0x00400000; //  4194304
-		internal const int META_I_BINDING_BLAST    = 0x00800000; //  8388608
-
 
 		/// <summary>
 		/// 
@@ -276,36 +301,38 @@ namespace nwn2_ai_2da_editor
 		/// </summary>
 		void SetFieldCols()
 		{
-			_fieldcols.Add("[School]");				//  0
-			_fieldcols.Add("[Range]");				//  1
-			_fieldcols.Add("[VS]");					//  2
-			_fieldcols.Add("[MetaMagic]");			//  3
-//			_fieldcols.Add("[TargetType]");			//  4
-			_fieldcols.Add("[Bard]");				//  5
-			_fieldcols.Add("[Cleric]");				//  6
-			_fieldcols.Add("[Druid]");				//  7
-			_fieldcols.Add("[Paladin]");			//  8
-			_fieldcols.Add("[Ranger]");				//  9
-			_fieldcols.Add("[Wiz_Sorc]");			// 10
-			_fieldcols.Add("[Warlock]");			// 11
-			_fieldcols.Add("[Innate]");				// 12
-			_fieldcols.Add("[ImmunityType]");		// 13
-			_fieldcols.Add("[ItemImmunity]");		// 14
-			_fieldcols.Add("[SubRadSpell1]");		// 15
-			_fieldcols.Add("[SubRadSpell2]");		// 16
-			_fieldcols.Add("[SubRadSpell3]");		// 17
-			_fieldcols.Add("[SubRadSpell4]");		// 18
-			_fieldcols.Add("[SubRadSpell5]");		// 19
-			_fieldcols.Add("[Category]");			// 20
-			_fieldcols.Add("[Master]");				// 21
-			_fieldcols.Add("[UserType]");			// 22
-			_fieldcols.Add("[UsesConcentration]");	// 23
-			_fieldcols.Add("[SpontaneouslyCast]");	// 24
-			_fieldcols.Add("[SpontCastClassReq]");	// 25
-			_fieldcols.Add("[HostileSetting]");		// 26
-			_fieldcols.Add("[FeatID]");				// 27
-			_fieldcols.Add("[AsMetaMagic]");		// 28
-			_fieldcols.Add("[REMOVED]");			// 30
+			int i = -1;
+
+			_fieldcols.Add("[School]");            FIELD_School            = ++i;
+			_fieldcols.Add("[Range]");             FIELD_Range             = ++i;
+			_fieldcols.Add("[VS]");                FIELD_VS                = ++i;
+			_fieldcols.Add("[MetaMagic]");         FIELD_MetaMagic         = ++i;
+//			_fieldcols.Add("[TargetType]");        FIELD_TargetType        = ++i;
+			_fieldcols.Add("[Bard]");              FIELD_Bard              = ++i;
+			_fieldcols.Add("[Cleric]");            FIELD_Cleric            = ++i;
+			_fieldcols.Add("[Druid]");             FIELD_Druid             = ++i;
+			_fieldcols.Add("[Paladin]");           FIELD_Paladin           = ++i;
+			_fieldcols.Add("[Ranger]");            FIELD_Ranger            = ++i;
+			_fieldcols.Add("[Wiz_Sorc]");          FIELD_Wiz_Sorc          = ++i;
+			_fieldcols.Add("[Warlock]");           FIELD_Warlock           = ++i;
+			_fieldcols.Add("[Innate]");            FIELD_Innate            = ++i;
+			_fieldcols.Add("[ImmunityType]");      FIELD_ImmunityType      = ++i;
+			_fieldcols.Add("[ItemImmunity]");      FIELD_ItemImmunity      = ++i;
+			_fieldcols.Add("[SubRadSpell1]");      FIELD_SubRadSpell1      = ++i;
+			_fieldcols.Add("[SubRadSpell2]");      FIELD_SubRadSpell2      = ++i;
+			_fieldcols.Add("[SubRadSpell3]");      FIELD_SubRadSpell3      = ++i;
+			_fieldcols.Add("[SubRadSpell4]");      FIELD_SubRadSpell4      = ++i;
+			_fieldcols.Add("[SubRadSpell5]");      FIELD_SubRadSpell5      = ++i;
+			_fieldcols.Add("[Category]");          FIELD_Category          = ++i;
+			_fieldcols.Add("[Master]");            FIELD_Master            = ++i;
+			_fieldcols.Add("[UserType]");          FIELD_UserType          = ++i;
+			_fieldcols.Add("[UsesConcentration]"); FIELD_UsesConcentration = ++i;
+			_fieldcols.Add("[SpontaneouslyCast]"); FIELD_SpontaneouslyCast = ++i;
+			_fieldcols.Add("[SpontCastClassReq]"); FIELD_SpontCastClassReq = ++i;
+			_fieldcols.Add("[HostileSetting]");    FIELD_HostileSetting    = ++i;
+			_fieldcols.Add("[FeatID]");            FIELD_FeatID            = ++i;
+			_fieldcols.Add("[AsMetaMagic]");       FIELD_AsMetaMagic       = ++i;
+			_fieldcols.Add("[REMOVED]");           FIELD_REMOVED           = ++i;
 		}
 
 		/// <summary>
