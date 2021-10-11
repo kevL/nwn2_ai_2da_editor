5a9c7ae773dda1bde1964612636180cf7d17d040
kevL <k3v1s73n@gmail.com>
2021-10-11 06:46:01 -0600

    HenchControl - doc and format

--- HenchControl.cs
+++ HenchControl.cs
@@ -6,24 +6,27 @@ using System.Windows.Forms;
 namespace nwn2_ai_2da_editor
 {
 	/// <summary>
-	/// A parent-class inherited by <see cref="tabcontrol_Spells"/>,
-	/// <see cref="tabcontrol_Racial"/>, and <see cref="tabcontrol_Classes"/>.
-	/// This allows the 'he.HenchControl' variable to call these functions
-	/// (incl/ the stuff in <see cref="UserControl"/>) without the need for
-	/// explicit casts.
-	/// @note Do not turn this into an abstract class since the 'tabcontrol_*'
-	/// designers will fuck up. thanks.
-	/// @note Do not turn this into an interface either since 'tabcontrol_Spells'
-	/// does not override the last two functs. (read: it doesn't need to, so
-	/// don't. your wel)
+	/// A parent-class inherited by <c><see cref="tabcontrol_Spells"/></c>,
+	/// <c><see cref="tabcontrol_Racial"/></c>, and
+	/// <c><see cref="tabcontrol_Classes"/></c>. This allows the
+	/// <c><see cref="he.HenchControl">he.HenchControl</see></c> variable to
+	/// call these functions (incl/ the stuff in
+	/// <c><see cref="UserControl"/></c>) without the need for explicit casts.
 	/// </summary>
+	/// <remarks>Do not turn this into an <c>abstract class</c> since the
+	/// <c>tabcontrol_*</c> designers will fuck up. thanks.
+	/// 
+	/// 
+	/// Do not turn this into an <c>interface</c> either since
+	/// <c>tabcontrol_Spells</c> does not override the last two functs. (read:
+	/// it doesn't need to, so don't. your wel)</remarks>
 	class HenchControl
 		: UserControl
 	{
 		internal virtual void SelectId(){}
 
 		internal virtual string GetMasterText(){ return String.Empty; }
-		internal virtual void SetMasterText(string text){}
+		internal virtual void   SetMasterText(string text){}
 
 		internal virtual void SelectResetButton(){}
 		internal virtual void SetResetColor(Color color){}
